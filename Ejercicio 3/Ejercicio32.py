7019f6e5327a121aee6dd4d4527a2117317e01c7
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-07 18:26:52 +0200

    gg

--- Ejercicio 3/Ejercicio32.py
+++ Ejercicio 3/Ejercicio32.py
@@ -1,9 +1,9 @@
 import numpy as np
 matriz=np.arange(0,100,9).reshape(3,4)
 print(len(matriz))
-for i in range(len(matriz)):
-    for j in range(len(matriz+1)):
-        if matriz[i][j]%5==0:
+for i in range(0,len(matriz)):
+    for j in range(len(matriz)+1):
+        if matriz[i][j]%5==0 and matriz[i][j]>=0:
             pass
         else:
             matriz[i][j]=-1
