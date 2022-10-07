3f125539c637a959191f38caa87e2178b8db0aa2
Sbastdia <sbastdia@myuax.com>
2022-10-07 18:38:24 +0200

    probando

--- Ejercicio 1/Ejercicio11.py
+++ Ejercicio 1/Ejercicio11.py
@@ -1,1 +1,14 @@
-lista = [1, .3, 'adsa', [1,4], (2, 5), {1, 2, 3}, {'a': 1, 'b': 2}]
+#Definimos una lista que contenga al menos 4 elementos de todos los tipos de valores permitidos en Python
+lista=[1, "abc", 3.4, -5, 6]
+lista2=[]
+#for i in reversed(lista):
+
+    #lista2.append(i)
+
+lista[0], lista[len-1]= lista[len-1], lista[0]
+lista=lista.pop()
+nuevaLista= [lista]
+print(lista)
+
+
+
