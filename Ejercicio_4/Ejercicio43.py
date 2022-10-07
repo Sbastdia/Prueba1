5d8894b06043ef36a58238127b47362c6434a06d
Enrique Marina <eherraiz@myuax.com>
2022-10-07 19:14:05 +0200

    sgds

--- Ejercicio_4/Ejercicio43.py
+++ Ejercicio_4/Ejercicio43.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-columnas = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']
-df = pd.read_csv('Ejercicio_4/auto-mpg.csv', sep='\s+')
 
-print(df)
+df = pd.read_csv('Ejercicio_4/auto-mpg.csv', sep='\s+', names=, columns = np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
+
+columns = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']
+
+print(df.columns)
