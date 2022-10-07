0d2a86307e130893e36f7c00d6c04411ace0cc0d
Enrique Marina <eherraiz@myuax.com>
2022-10-07 18:01:00 +0200

    af

--- Ejercicio 2/Ejercicio21.py
+++ Ejercicio 2/Ejercicio21.py
@@ -1,4 +1,6 @@
 lista = [18, 50, 210, 80, 145, 333, 70, 30]
 for i in range(len(lista)):
+    if lista[i] > 300:
+        break
     if lista[i] % 10 == 0 and lista[i] < 200:
         print(lista[i])
