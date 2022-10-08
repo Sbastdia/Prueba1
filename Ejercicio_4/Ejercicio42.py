76ca8db95c7c75ace888875df5747f79b49fb021
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-08 18:51:20 +0200

    gg

--- Ejercicio_4/Ejercicio42.py
+++ Ejercicio_4/Ejercicio42.py
@@ -12,13 +12,57 @@ def convertir(array):
 print(convertir(data_array))
 
 list1=np.array([10, True, 8.00, False, 8, 10])
-h=convertir(list1)
-print(h)
+new=convertir(list1)
 
-for i in h:
+print(new)
+
+for i in new:
     print(type(i))
 
-plt.plot(h)
+new2=pd.DataFrame(new,columns=['ej_42'])
+print(new2.memory_usage())
+
+plt.plot(new)
 plt.show()
 
-print(h.describe())
+print(new.describe())
+
+def frecuencia_valor(serie,valor):
+    contador=0
+    valores=[]
+    for i in range(len(serie)):
+        if serie[i] not in valores:
+            valores.append(serie[i])
+    if valor in valores:
+        for i in range(len(serie)):
+            if valor==serie[i]:
+                contador+=1
+            else:pass
+        return contador
+    else:
+        return f'{valor} no es un valor de la serie'
+
+def frecuencia_serie(serie):
+    dict={}
+    for i in range(len(serie)):
+        dict[serie[i]]=frecuencia_valor(serie,serie[i])
+    return dict
+
+print(frecuencia_serie(new))
+
+def valores_unicos(serie,valores=[],repetidos=[],unicos=[]):
+    for i in range(len(serie)):
+        if serie[i] not in valores:
+            valores.append(serie[i])
+        else:
+            repetidos.append(serie[i])
+    for i in valores:
+        if i not in repetidos:
+            unicos.append(i)
+    return unicos
+print(valores_unicos(new))
+
+
+
+
+
