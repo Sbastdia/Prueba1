9b5ef08423dcfb9cdf1deb2e5974d3136c5aaa73
Albertojserr <alberto.j.serrano@gmail.com>
2022-10-08 19:12:37 +0200

    Ejercicio 4.1 en clases

--- Ejercicio_4/Ejercicio41.py
+++ Ejercicio_4/Ejercicio41.py
@@ -1,8 +1,25 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = list(range(1, 10))
+
+'''x = list(range(1, 10))
 y = np.ones(9)
 
 plt.plot(x, y, 'o', color='red')
-plt.show()
+plt.show()'''
+class Ejercicio41:
+    def __init__(self,menor,mayor):
+        self.dist=(mayor-menor)/100
+    @staticmethod
+    def ejecutar():
+        ejercicio41=Ejercicio41(0,10)
+        x = list(np.arange(ejercicio41.dist, 10,ejercicio41.dist))
+        y = list()
+        for i in x:
+            y.append(i**3)
+
+        plt.plot(x, y, 'o', color='red')
+        plt.show()
+
+if __name__=="__main__":
+    Ejercicio41.ejecutar()
