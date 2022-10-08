7e0a185fdffb5f51f3039e0cd508fcbabccb70e8
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-08 19:15:25 +0200

    gg

--- Ejercicio_4/Ejercicio42.py
+++ Ejercicio_4/Ejercicio42.py
@@ -3,8 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data_array = np.array([1.0,2,3,4])
-print(data_array)
-
+#4.2.1
 def convertir(array):
     array=array.astype(int)
     data_df = pd.Series(array)
@@ -14,11 +13,13 @@ print(convertir(data_array))
 new_serie = pd.Series(map(lambda x: int(x), pd.Series(data_array)))
 print(new_serie)
 
+#4.2.2
 data_array2=np.array([10, True, 8.00, False, 8, 10])
 new=convertir(data_array2)
 new2 = pd.Series(map(lambda x: int(x), pd.Series(data_array2)))
-print(new2)
 
+#4.2.3 y 4.2.9
+print(new2)
 print(new)
 
 for i in new:
@@ -27,11 +28,14 @@ for i in new:
 new2=pd.DataFrame(new,columns=['ej_42'])
 print(new2.memory_usage())
 
+#4.2.4
 plt.plot(new)
 plt.show()
 
+#4.2.5
 print(new.describe())
 
+#4.2.6
 def frecuencia_valor(serie,valor):
     contador=0
     valores=[]
@@ -55,6 +59,7 @@ def frecuencia_serie(serie):
 
 print(frecuencia_serie(new))
 
+#4.2.7
 def valores_unicos(serie,valores=[],repetidos=[],unicos=[]):
     for i in range(len(serie)):
         if serie[i] not in valores:
@@ -67,6 +72,7 @@ def valores_unicos(serie,valores=[],repetidos=[],unicos=[]):
     return unicos
 print(valores_unicos(new))
 
+#4.2.8
 def ordenar(serie):
     n = len(serie)
     for i in range(n):
