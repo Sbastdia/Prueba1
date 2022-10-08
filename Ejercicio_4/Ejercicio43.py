b13e43dcc5d44a0429f21723188a0fca87690737
Enrique Marina <eherraiz@myuax.com>
2022-10-08 23:47:00 +0200

    helado de vainilla

--- Ejercicio_4/Ejercicio43.py
+++ Ejercicio_4/Ejercicio43.py
@@ -2,6 +2,25 @@ import numpy as np
 import pandas as pd
 
 
+
 df = pd.read_csv('Ejercicio_4/auto-mpg.csv', sep='\s+', names= np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
 
-print(df.head())
+t = df.loc[:, 'Weight']
+
+#print(t.head())
+#print(df.head())
+
+
+for i in range(len(df)):
+    df[i] 
+
+for df[]
+
+
+
+
+for  c in range(len(t)):
+    if t[c]<5000.0 and t[c]==float:
+        t=t.drop(c)
+
+print(t)
