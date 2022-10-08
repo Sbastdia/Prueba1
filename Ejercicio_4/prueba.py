ddfac098f3d4cfd08dcd5182a7231345a643ca8d
Enrique Marina <eherraiz@myuax.com>
2022-10-08 23:52:14 +0200

    f

--- Ejercicio_4/prueba.py
+++ Ejercicio_4/prueba.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv('Ejercicio_4/auto-mpg.csv', sep='\s+', names= np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
 
-df = df.DataFrame(df)
+df = pd.DataFrame(df)
 t = df.loc[:, 'Weight']
 
 #print(t.head())
@@ -12,7 +12,8 @@ t = df.loc[:, 'Weight']
 
 
 for i in range(len(df)):
-    print(type(df.iloc[i]))
+    print(i)
+    print(type(pd.ilo))
 
 
 
