aacfd9c2cc4e9bb32c6a3685511e2a0f7ea7f831
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-09 00:11:22 +0200

    gg

--- Ejercicio_4/Ejercicio43.py
+++ Ejercicio_4/Ejercicio43.py
@@ -2,6 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv('Ejercicio_4/auto-mpg.csv', sep='\s+', names= np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
+df = pd.read_csv('auto-mpg.csv', sep='\s+', names= np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
 
 print(df.head())
+
+new_df=df['Displacement']
+list=[]
+for i in range(len(new_df)):
+    if new_df.iloc[i]<=300 and new_df.iloc[i]==float:
+        list.append(i)
+df=df.drop(list)
+print(list)
