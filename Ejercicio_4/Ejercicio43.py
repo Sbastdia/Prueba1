5189fe356d70ea14f6186e458077d9c636b1dec7
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-09 13:38:00 +0200

    gg

--- Ejercicio_4/Ejercicio43.py
+++ Ejercicio_4/Ejercicio43.py
@@ -3,26 +3,22 @@ import pandas as pd
 
 
 df = pd.read_csv('auto-mpg.csv', sep='\s+', names= np.array(['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car_name']))
-
-t = df.loc[:, 'Weight']
-t=t.astype(float)
+print(df)
 #print(t.head())
 #print(df.head())
+def filtro_peso_minimo(dtf,peso):
+    for c in range(len(dtf['Weight'])):
+        if dtf['Weight'][c]<=peso:
+            dtf=dtf.drop(c)
+    return dtf
+print(filtro_peso_minimo(df,5000))
 
+def filtro_antiguedad(dtf,año):
+    for c in range(len(dtf['Model Year'])):
+        if dtf['Model Year'][c]!=año:
+            dtf=dtf.drop(c)
+    return dtf
 
-
-"""for i in range(len(df)):
-    df[i] 
-
-for df[]"""
-
-
-
-
-for  c in range(len(t)):
-    if t.iloc[c]<3000.0 :
-        t=t.drop(c)
-print(type(t))
-
-
+new_df=filtro_antiguedad(df,76)
+print(new_df)
 
