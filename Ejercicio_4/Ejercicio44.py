31d504c4fae8500850a3db97f3aab048281d5ae4
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-11 01:52:03 +0200

    gg

--- Ejercicio_4/Ejercicio44.py
+++ Ejercicio_4/Ejercicio44.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
-class Ejercicio44:
-    #concatenar dos tablas
-    auto-mpg.csv= pd.concat([])
+df1=pd.read_csv('weatherHistory.csv')
+df1=df1.dropna()
+df1.reset_index(drop=True, inplace=True)
+df2=pd.read_csv('Dataset_FUERTEVENTURA.csv')
+df2=df2.dropna()
+df2.reset_index(drop=True, inplace=True)
+print(df2['atmos_pres'])
