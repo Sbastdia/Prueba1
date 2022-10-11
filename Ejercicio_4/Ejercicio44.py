5b7f608051923d0c91ca730f124fb20a8d0f0828
Albertojserr <alberto.j.serrano@gmail.com>
2022-10-11 17:14:59 +0200

    empezando a juntar las cosas

--- Ejercicio_4/Ejercicio44.py
+++ Ejercicio_4/Ejercicio44.py
@@ -8,3 +8,12 @@ df2=pd.read_csv('Dataset_FUERTEVENTURA.csv')
 df2=df2.dropna()
 df2.reset_index(drop=True, inplace=True)
 print('Holi encontre estos dataset de temas metereologicos con cositas en comun que alguien siga que me da mucha pereza')
+
+
+class Ejercicio44:
+    def __init__(self,data1,data2):
+        self.df1=data1
+        self.df2=data2
+    def juntar(self):
+        self.dataframe=pd.concat(self.df1,self.df2)
+    
